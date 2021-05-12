26f4a3cdfcf17df5e28c918bb1a87b970091b27d
Christian Kruse <christian.kruse.bt@gmail.com>
2021-05-12 19:15:29 +0200

    save query to mongodb

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,4 +1,6 @@
 from fastapi import Header, HTTPException
+from pymongo import MongoClient
+from loguru import logger
 
 #async def get_token_header(x_token: str = Header(...)):
 #    if x_token != "fake-super-secret-token":
@@ -8,3 +10,82 @@ from fastapi import Header, HTTPException
 #async def get_query_token(token: str):
 #    if token != "jessica":
 #        raise HTTPException(status_code=400, detail="No Jessica token provided")
+
+class Helper:
+    def __init__(self):
+        self.data = []
+
+    def getMongoDbClient(self):
+        return MongoClient('mongodb', 27017)
+
+    def entrez_search_pubmed(self, query,records_per_query=10,email="XXX@YYY.com"):
+        from Bio import Entrez
+        Entrez.email = email
+        # Search
+        handle = Entrez.esearch(db="pubmed",term=query, idtype="acc")
+        record = Entrez.read(handle)
+        handle.close()
+        return record
+
+    def entrez_search_pubmed_parse_to_uid_list(self, query_result):
+        result_parsed = [dict({'uid': value}) for value in query_result['IdList'] ]
+        return result_parsed
+    
+    def entrez_fetch_list_summary(self, uid_list,email="XXX@YYY.com"):
+        from Bio import Entrez
+        Entrez.email = email
+        results = [ Entrez.read( Entrez.esummary(db="pubmed", id=uid) ) for uid in uid_list ]
+        return [{'uid':a, 'summary':b} for a,b in zip(uid_list, results)]
+        return results
+
+    def flatten_abstract(self, abstract_xml):
+        abstract = ''
+        for abstractText in abstract_xml.find_all('abstracttext'):
+            if abstractText.get('label') != None:
+                abstract = abstract + " " + abstractText.get('label') + ": "
+            abstract = abstract + abstractText.text
+        return abstract
+
+    def entrez_fetch_abstracts(self, uid,email):
+        from Bio import Entrez
+        from bs4 import BeautifulSoup as bs
+        Entrez.email = email
+        handle = Entrez.efetch(db="pubmed", id=uid, rettype='Medline', retmode='xml')
+        result = handle.readlines()
+        result = b"".join(result)
+        bs_content = bs(result, "lxml")
+        abstracts = bs_content.find_all('abstract')
+        handle.close()
+        # Abstract
+        results = [ self.flatten_abstract(abstract) for abstract in abstracts]
+        return results
+        
+    def entrez_construct_abstract_dict(self, uids,email="XXX@YYY.com"):
+        results = self.entrez_fetch_abstracts( uids, email )
+        return [{'uid':a, 'abstract':b} for a,b in zip(uids, results)]
+
+    def entrez_fetch_full_text_linkout(self, uid_list):
+        import requests
+        query = ",".join(uid_list)
+        result = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&id=" + query + "&cmd=prlinks&retmode=json").json()
+        results_parsed = result['linksets'][0]['idurllist']
+        return [{ 'uid': result['id'], 'objurls' : result['objurls']} for result in results_parsed]
+
+
+    ### Query databases
+    def mongodb_find_all_uid(self):
+        client = self.getMongoDbClient()
+        return list( client['Entrez']['uid_list'].find({},{"_id" : False }) )
+
+    def mongodb_find_all_summaries(self):
+        client = self.getMongoDbClient()
+        return list( client['Entrez']['summaries'].find({},{"_id" : False }) )
+
+    def mongodb_find_all_abstracts(self):
+        client = self.getMongoDbClient()
+        return list( client['Entrez']['abstracts'].find({},{"_id" : False }) )
+
+    def mongodb_find_all_elinks(self):
+        client = self.getMongoDbClient()
+        return list( client['Entrez']['elinks'].find({},{"_id" : False }) )
+        
---
+++ src/api/routers/mongodb_entrez.py
@@ -0,0 +1,119 @@
+from fastapi import Body, APIRouter
+from pymongo import MongoClient
+from pydantic import BaseModel
+from loguru import logger
+from ..dependencies import Helper
+
+router = APIRouter()
+
+Helper_ = Helper()
+
+################################################
+### MODELS
+################################################
+
+class EntrezQuery(BaseModel):
+    query: str
+    email: str
+
+################################################
+### QUERY
+################################################
+
+@router.post("/mongodb_entrez/clean_dictionaries/", tags=["MongoDB Entrez"], summary="Erase all content of Entrez-MongoDB dictionaries")
+async def clean_mongodb_entrez_dictionaries():
+    client = Helper_.getMongoDbClient()
+    db = client['Entrez']
+    uid_list = db['uid_list'] 
+    uid_list = []
+    summaries = db['summaries']
+    summaries = []
+    abstracts = db['abstracts']
+    abstracts = []
+    elinks = db['elinks']
+    elinks = []
+    counts = {  'uid_list' : db['uid_list'].find().count(),
+                'summaries' : db['summaries'].find().count(),
+                'abstracts' : db['abstracts'].find().count(),
+                'elinks' : db['elinks'].find().count()
+    }
+    return counts
+
+@router.post("/mongodb_entrez/generate_dictionaries/", tags=["MongoDB Entrez"], summary="Generate empty Entrez dictionaries in MongoDB ")
+async def generate_empty_mongodb_entrez_dictionaries():
+    client = Helper_.getMongoDbClient()
+    db = client['Entrez']
+    uid_list = db['uid_list'] 
+    uid_list = []
+    summaries = db['summaries']
+    summaries = []
+    abstracts = db['abstracts']
+    abstracts = []
+    elinks = db['elinks']
+    elinks = []
+    counts = {  'uid_list' : db['uid_list'].find().count(),
+                'summaries' : db['summaries'].find().count(),
+                'abstracts' : db['abstracts'].find().count(),
+                'elinks' : db['elinks'].find().count()
+    }
+    return counts
+
+@router.post("/mongodb_entrez/save_query_results/", tags=["MongoDB Entrez"], summary="Perform an Entrez query and save results to MongoDB")
+async def save_query_results_to_mongodb(query_body: EntrezQuery = Body(...,example={ "query": "kruse eiken vestergaard", 'email' : "XXX@YYY.com" } ) ):
+    client = Helper_.getMongoDbClient()
+    db = client['Entrez']
+
+    # Query
+    results_query = Helper_.entrez_search_pubmed(query = query_body.query, email = query_body.email )
+    results_query_parsed = Helper_.entrez_search_pubmed_parse_to_uid_list(results_query)
+    uid_list = results_query['IdList']
+    logger.info(" Query ")
+
+    # UIDs
+    results_existing = Helper_.mongodb_find_all_uid()
+    results_combined = results_query_parsed + results_existing
+    results_combined = list( {v['uid']:v for v in results_combined}.values() )
+    db['uid_list'].remove()
+    db['uid_list'].insert_many( results_combined )
+    logger.info(" UIDs ")
+
+
+    # Summaries
+    results_existing = Helper_.mongodb_find_all_summaries()
+    results_summaries = Helper_.entrez_fetch_list_summary(uid_list = uid_list)
+    results_combined = results_summaries + results_existing
+    results_combined = list( {v['uid']:v for v in results_combined}.values() )
+    db['summaries'].remove()
+    db['summaries'].insert_many( results_combined )
+    logger.info(" Summaries ")
+
+
+    # Abstracts
+    results_existing = Helper_.mongodb_find_all_abstracts()
+    results_abstracts = Helper_.entrez_construct_abstract_dict(uids = uid_list)
+    results_combined = results_abstracts + results_existing
+    results_combined = list( {v['uid']:v for v in results_combined}.values() )
+    db['abstracts'].remove()
+    db['abstracts'].insert_many( results_combined )
+    logger.info(" Abstracts ")
+
+
+    # Elink
+    results_existing = Helper_.mongodb_find_all_elinks()
+    results_elinks = Helper_.entrez_fetch_full_text_linkout(uid_list)
+    results_combined = results_elinks + results_existing
+    results_combined = list( {v['uid']:v for v in results_combined}.values() )
+    db['elinks'].remove()
+    db['elinks'].insert_many( results_combined )
+    logger.info(" Elinks ")
+
+
+    # Counts
+    counts = {  'uid_list' : db['uid_list'].find().count(),
+            'summaries' : db['summaries'].find().count(),
+            'abstracts' : db['abstracts'].find().count(),
+            'elinks' : db['elinks'].find().count()
+    }
+
+    return counts
+
