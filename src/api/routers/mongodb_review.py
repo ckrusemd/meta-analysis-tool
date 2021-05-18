7a934c1b2851723c82e4014c3049bc6cb9683849
Christian Kruse <christian.kruse.bt@gmail.com>
2021-05-18 13:51:50 +0200

    api get next abstract by project_id

--- src/api/routers/mongodb_review.py
+++ src/api/routers/mongodb_review.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from pydantic import BaseModel
 from loguru import logger
 from ..dependencies import Helper
+import random
 
 router = APIRouter()
 
@@ -46,3 +47,41 @@ async def mongodb_review_uid(project_id: int, user_id: int, uid: int, include: b
     
     return result
 
+
+@router.get("/mongodb_review/next_abstract/{project_id}/", tags=["MongoDB Review"], summary="Get Next Unreviewed Abstract")
+async def mongodb_next_abstract(project_id: int):
+    client = Helper_.getMongoDbClient()
+
+    ## Project UID list
+    db = client['Projects']
+    project_uids = db['project_uids']
+    uid_list = list( project_uids.find({'project_id' : project_id },{"_id":0}) )[0]['uid_list']
+    logger.info( uid_list ) 
+
+    ## Project review UIDs
+    result = list( db['projects'].find({ 'uid' : project_id },{"_id":0}) ) 
+    logger.info( result ) 
+
+    ## Diff
+    diff = list(set(uid_list) - set(result))
+    logger.info( diff ) 
+
+    ## Random selection
+    selection = random.choice(diff)
+
+    ## Abstract elements
+    entrez = client['Entrez']
+    # Summary
+    summary = entrez['summaries'].find_one({ 'uid' : selection },{"_id":0})
+    # Abstract
+    abstract = entrez['abstracts'].find_one({ 'uid' : selection },{"_id":0})
+    # Elink
+    elinks = entrez['elinks'].find_one({ 'uid' : selection },{"_id":0})
+
+    ## Compile
+    result = {  'uid' : selection,
+                'summary' : summary,
+                'abstract' : abstract,
+                'elinks' : elinks }
+
+    return result
