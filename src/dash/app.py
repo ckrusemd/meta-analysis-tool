adf1ac9fbaa915df9bda3474a8483b29b1549bf6
Christian Kruse <christian.kruse.bt@gmail.com>
2021-05-18 13:00:33 +0200

    get current project

--- src/dash/app.py
+++ src/dash/app.py
@@ -4,6 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import requests
+from loguru import logger
 
 # Fontawesome (icons)
 FA = "https://use.fontawesome.com/releases/v5.8.1/css/all.css"
@@ -25,7 +26,7 @@ menu_1 = [
         dbc.Row(
             [
                 dbc.Col(    html.I(className="fas fa-table mr-3"), width=1, className="icon-padding" ),
-                dbc.Col(    dbc.NavLink("Page 1", href="/page-1/"), className="navlink-padding"  )
+                dbc.Col(    dbc.NavLink("Create Project", href="/create_project/"), className="navlink-padding"  )
             ],
             className="my-1",
         ),
@@ -39,7 +40,7 @@ menu_2 = [
         dbc.Row(
             [
                 dbc.Col(    html.I(className="fas fa-chart-line mr-3"), width=1, className="icon-padding"  ),
-                dbc.Col(    dbc.NavLink("Page 2", href="/page-2/"), className="navlink-padding"  ),
+                dbc.Col(    dbc.NavLink("Current Project", href="/current_project/"), className="navlink-padding"  ),
             ],
             className="my-1",
         ),
@@ -61,28 +62,49 @@ sidebar = html.Div(
 # Nav Bar Row
 navbarRow = dbc.Row(
     [
+        dbc.Col( dbc.NavbarBrand("Current Project",className="navbar-offset") ),
+        dbc.Col( dcc.Dropdown(
+            id='current-project',
+            options=[
+                {'label': 'Kruse Eiken Vestergaard', 'value': 'Kruse Eiken Vestergaard'}
+            ],
+            value='Kruse Eiken Vestergaard'
+        ),width=6),
+        dbc.Col( dbc.NavbarBrand("Current User",className="navbar-offset") ),
+        dbc.Col(dcc.Dropdown(
+            id='demo-dropdown',
+            options=[
+                {'label': 'Christian Kruse', 'value': 'Christian Kruse'}
+            ],
+            value='Christian Kruse'
+        ),width=6)
     ],
     no_gutters=True,
-    className="ml-auto flex-nowrap mt-3 mt-md-0",
+    className="ml-4 flex-nowrap",
     align="center",
 )
 
 # Nav Bar
 navbar = dbc.Navbar(
     [
-        html.A(
-            dbc.Row(
-                [
-                    dbc.Col(html.Img(src=LOGO, height="30px")),
-                    dbc.Col(dbc.NavbarBrand("Dash Template", className="ml-2")),
-                ],
-                align="center",
-                no_gutters=True,
-            ),
-            href="https://github.com/ckrusemd",
-        ),
-        dbc.NavbarToggler(id="navbar-toggler"),
-        dbc.Collapse(navbarRow, id="navbar-collapse", navbar=True),
+        dbc.Col(
+            html.A(
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src=LOGO, height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Meta-Analysis Tool", className="ml-2 flex-nowrap")),
+                        
+                    ],
+                    align="center",
+                    no_gutters=True,
+                ),
+                href="https://github.com/ckrusemd",
+            ),width=2),
+        dbc.Col(width=4),
+        dbc.Col(
+            dbc.Row([
+                dbc.Collapse(navbarRow, id="navbar-collapse", navbar=True)
+            ]),width=6),
     ],
     color="dark",
     dark=True,
@@ -96,25 +118,42 @@ app.layout = html.Div([dcc.Location(id="url"), navbar,sidebar, content])
 
 ### Page Layouts
 
-# Page 1
+# Create Project
 page_1 = html.Div([
-    html.H1("Page 1"),
-    html.Button('Test API', id='test-api', n_clicks=0),
+    html.H1("Create Project"),
+    html.Button('Test Api', id='test-api', n_clicks=0),
     html.Div(id='container-api-result',
              children='')
 ])
 
-# Page 2
+# Current Project
 page_2 = html.Div([
-    html.H1("Page 2")
+    html.H1("Current Project"),
+    html.Button("Get Current Project", id='get-current-project', n_clicks=0),
+    html.Div(id="current-project-content")
 ])
+
+
+# Get Current Project
+@app.callback(
+    dash.dependencies.Output('current-project-content', 'children'),
+    [dash.dependencies.Input('get-current-project', 'n_clicks')])
+def get_current_project(n_clicks):
+    projects = requests.get("http://api:8080/mongodb_projects/projects").json()
+    logger.info( projects )
+    result = []
+    for project in projects:
+        for key,value in project.items():
+            result = result + [html.P(key + ": " + str( value ) )]
+    return result
+
 
 # URL
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
-    if pathname in ["/", "/page-1/"]:
+    if pathname in ["/", "/create_project/"]:
         return page_1
-    elif pathname == "/page-2/":
+    elif pathname == "/current_project/":
         return page_2
     return dbc.Jumbotron(
         [
@@ -129,7 +168,7 @@ def render_page_content(pathname):
     dash.dependencies.Output('container-api-result', 'children'),
     [dash.dependencies.Input('test-api', 'n_clicks')])
 def update_output(n_clicks):
-    greeting = requests.get("http://127.0.0.1:80/test/HelloWorld").json()
+    greeting = requests.get("http://api:8080/test/HelloWorld").json()
     return html.H2(greeting)
 
 # Run the server
